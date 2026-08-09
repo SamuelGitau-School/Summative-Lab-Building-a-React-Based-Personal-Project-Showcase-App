969a0cc6041d6366a178174600532ec891a6836d
SamuelGitau_School <samuel.gitau4@student.moringaschool.com>
2026-08-09 06:37:30 +0300

    added routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProtectedRoute from './assests/Auth/ Protectedroute'
+import Login from './components/Form page/Login/Login'
+import Signup from './components/Form page/Sign up/Sign-up'
+import Dashboard from './components/Dashboard/Dashboard'
+import AdminPanel from './components/Dashboard/AdminPanel'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Route>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+
+      <Route path='/dashboard' element ={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+      <Route path='/admin' element={<ProtectedRoute><AdminPanel/></ProtectedRoute>}/>
+      
+    </Route>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
